9cc6d88f39b72c959c9f9e7edb93a7ed764ee36a
carolina <ci.arayagonzalez@gmail.com>
2024-05-16 00:50:16 -0400

    add items data and card component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,25 @@
 import React from "react"
+import storeItems  from "../data/items.json"
+import { Row, Col } from "react-bootstrap"
+//components
+import { StoreItem } from "../components/StoreItem"
 
 export function Store() {
     return (
+        <>
         <h1>Store</h1>
+        <Row xs={1} md={2} lg={3} className="g-3" >
+            {
+                storeItems.map( item => (
+                    <Col key={item.id} >
+                        <StoreItem {...item} />
+                    </Col>
+                )
+                    
+                )
+            }
+        </Row>
+        </>
+        
     )
 }
